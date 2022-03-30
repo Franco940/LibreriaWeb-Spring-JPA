178be8a0a15f5209b9be17641d1fbf131336944d
Franco <Franco@DESKTOP-N9EC72E>
2022-03-30 01:02:20 -0300

    Se creó el método para actualizar un registro de la tabla "prestamo". Se creó el método para actualizar la columna de alta de un prestamo. Se creó el método para buscar un préstamo con estado de alta. Se creó el método para buscar un préstamo con estado de baja.

--- src/main/java/com/ejercicio/libreria/repositorios/PrestamoRepositorio.java
+++ src/main/java/com/ejercicio/libreria/repositorios/PrestamoRepositorio.java
@@ -1,7 +1,13 @@
 package com.ejercicio.libreria.repositorios;
 
+import com.ejercicio.libreria.entidades.Libro;
 import com.ejercicio.libreria.entidades.Prestamo;
+import java.util.Date;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -12,4 +18,17 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PrestamoRepositorio extends JpaRepository<Prestamo, String>{
     
+    @Modifying
+    @Query("UPDATE Prestamo AS p SET p.alta = :alta WHERE p.id = :id")
+    public void actualizarAlta(@Param("id") String id, @Param("alta") boolean alta);
+
+    @Modifying
+    @Query("UPDATE Prestamo AS p SET p.fechaDevolucion = :fechaDevolucion, p.libro = :libro WHERE p.id = :id")
+    public void actualizarPrestamo(@Param("id") String id, @Param("fechaDevolucion") Date fechaDevolucion, @Param("libro") Libro libro);
+    
+    @Query("SELECT p FROM Prestamo AS p WHERE p.alta IS TRUE")
+    public List<Prestamo> buscarPrestamosDeAlta();
+    
+    @Query("SELECT p FROM Prestamo AS p WHERE p.alta IS NOT TRUE")
+    public List<Prestamo> buscarPrestamosDeBaja();
 }

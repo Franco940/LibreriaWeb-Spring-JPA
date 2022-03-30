8f840387a5f658529e7fc31cd48a6cf9bb5d6352
Franco <Franco@DESKTOP-N9EC72E>
2022-03-30 00:59:24 -0300

    Se creó el método para listar los prestamos dados de baja. Se creó el método para dar de baja un préstamo. Se creó el método para dar de alta un préstamo. Se creó el método para ir al formulario de edición de préstamo. Se creó el método para para editar un préstamo.

--- src/main/java/com/ejercicio/libreria/controladores/PrestamoControlador.java
+++ src/main/java/com/ejercicio/libreria/controladores/PrestamoControlador.java
@@ -1,16 +1,19 @@
 package com.ejercicio.libreria.controladores;
 
 import com.ejercicio.libreria.entidades.Libro;
+import com.ejercicio.libreria.entidades.Prestamo;
 import com.ejercicio.libreria.servicios.LibroServicio;
 import com.ejercicio.libreria.servicios.PrestamoServicio;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -30,8 +33,64 @@ public class PrestamoControlador {
     private LibroServicio libroServicio;
     
     @GetMapping("/lista")
-    public String index(){
+    public String index(ModelMap modelo){
+        
+        List<Prestamo> prestamos = prestamosServicio.prestamosDeAtla();
+        
+        modelo.put("prestamos", prestamos);
+        
         return "prestamo.html";
+    }
+    
+    @GetMapping("/baja")
+    public String listaPretamosDeBaja(ModelMap modelo){
+        
+        List<Prestamo> prestamos = prestamosServicio.prestamosDeBaja();
+        
+        modelo.put("prestamos", prestamos);
+        
+        return "prestamosDebaja.html";
+    }
+    
+    @GetMapping("/baja/{id}")
+    public String darDeBaja(@PathVariable String id){
+        prestamosServicio.actualizarAlta(id, false);
+        
+        return "redirect:/prestamo/lista";
+    }
+    @GetMapping("/alta/{id}")
+    public String darDeAlta(@PathVariable String id){
+        prestamosServicio.actualizarAlta(id, true);
+        
+        return "redirect:/prestamo/baja";
+    }
+    
+    @GetMapping("/editar/{id}")
+    public String formEditarPrestamo(ModelMap modelo, @PathVariable String id){
+        Optional<Prestamo> prestamo = prestamosServicio.buscarPrestamoPorId(id);
+        List<Libro> libros = libroServicio.buscarLibrosDeAlta();
+        
+        
+        modelo.put("prestamo", prestamo.get());
+        modelo.put("libros", libros);
+        
+        return "prestamoEditar.html";
+    }
+    
+    @PostMapping("/editar/{id}")
+    public String editarPrestamo(ModelMap modelo, @PathVariable String id, @RequestParam String idLibro, @RequestParam String fechaDevolucion){
+        try{
+            SimpleDateFormat formateador = new SimpleDateFormat("yyyy-MM-dd");
+            Date fechaDevolver = formateador.parse(fechaDevolucion);
+            
+            prestamosServicio.editarPrestamo(id, idLibro, fechaDevolver);
+            
+            modelo.put("exito", true);
+            return "prestamoEditar.html";
+        }catch(Exception e){
+            modelo.put("error", true);
+            return "prestamoEditar.html";
+        }
     }
     
     @GetMapping("/hacer")
